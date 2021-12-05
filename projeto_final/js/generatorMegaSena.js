f2d397c22d1c1de752395abcaa4a9d24635f57b8
Alfredo <alfredo.s.miranda@hotmail.com>
2021-12-05 20:34:52 -0300

    js incorporado

---
+++ projeto_final/js/generatorMegaSena.js
@@ -0,0 +1,38 @@
+        function sorteia() {
+
+            return Math.round(Math.random() * 60);
+            
+        }
+
+        function sorteiaNumeros() {
+            let megaSena = []
+            let numero = 1
+
+            while(numero <= 6) {
+
+                let numeroAleatorio = sorteia()
+                let achou = false
+
+                if(numeroAleatorio !== 0 && numeroAleatorio <= 60) {
+
+                    for(posicao = 0; posicao < megaSena.length; posicao++) {
+
+                        if(megaSena[posicao] == numeroAleatorio) {
+
+                            achou = true
+                            break
+                        }
+                    }
+
+                    if(achou == false) {
+
+                        megaSena.push(numeroAleatorio)
+                        numero++
+                    }
+
+                }
+
+            }
+
+            alert("Os números da sorte são: " + megaSena)
+        }
